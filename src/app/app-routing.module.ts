cacb77c7f2caae67590a9d9387c677b7c9d26c84
AlexF99 <alexandreopf@gmail.com>
2022-05-17 14:25:31 -0300

    setting projects component to be its own page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MonthlyPresentationComponent } from './monthly-presentation/monthly-presentation.component';
+import { ProjetosComponent } from './projetos/projetos.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'monthly', component: MonthlyPresentationComponent },
+  { path: 'projects', component: ProjetosComponent },
 ];
 
 @NgModule({
